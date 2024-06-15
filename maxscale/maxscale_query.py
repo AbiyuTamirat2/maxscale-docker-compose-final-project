15f88d036aac2d017b689c43b58d13563c51122e
AbiyuTamirat2 <148640715+AbiyuTamirat2@users.noreply.github.com>
2024-06-15 10:15:34 -0700

    Update docker-compose and maxscale.cnf

--- maxscale/maxscale_query.py
+++ maxscale/maxscale_query.py
@@ -1,1 +1,13 @@
+import pymysql
+from tabulate import tabulate
+
+# Connect to MaxScale
+db = pymysql.connect(
+    host="10.0.0.66",
+    port=4000,
+    user="maxuser",
+    passwd="maxpwd"
+)
+
+cursor = db.cursor()
 
